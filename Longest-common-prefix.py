e71fc96eb41664b36b5499c748eb8d5d249cbbdb
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-08-25 22:33:59 +0545

    initila still some errors, need to leave urgently

--- Longest-common-prefix.py
+++ Longest-common-prefix.py
@@ -1,1 +1,24 @@
 
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        word = strs[0]
+        j = - 1
+        for i in range(1, len(strs)):
+            for k, char in enumerate(word):
+                if k >= len(strs[i]):
+                    break
+                if char == strs[i][k]:
+                    j = k
+                else:
+                    if k != j:
+                        j = -1
+                        break
+            print("hello ", j)
+        if j == -1:
+            return ''
+        else:
+            return word[: j + 1]
