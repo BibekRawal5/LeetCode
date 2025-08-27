96387a5389e6c57474bee34cf6da7b181bb5e245
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-08-27 23:11:43 +0545

    Scrapped the previous idea, and implemented a new one

--- Longest-common-prefix.py
+++ Longest-common-prefix.py
@@ -4,29 +4,15 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        word = strs[0]
-        j = - 1
-        looped = False
-        for i in range(1, len(strs)):
-            looped = True
-            for k, char in enumerate(word):
-                print("K: ", k)
-                if k >= len(strs[i]):
-                    break
-                if char == strs[i][k]:
-                    j = k
-                else:
-                    if k != j:
-                        if k == 0:
-                            print(char)
-                            j = -1
-                            break
-                        else:
-                            break
-            print("hello ", j)
-        if not (looped):
-            return word
-        if j == -1:
-            return ''
-        else:
-            return word[: j + 1]
+        pref = strs[0]
+        pref_len = len(pref)
+
+        for s in strs[1:]:
+            while pref != s[0:pref_len]:
+                pref_len -= 1
+                if pref_len == 0:
+                    return ""
+                
+                pref = pref[0:pref_len]
+        
+        return pref
