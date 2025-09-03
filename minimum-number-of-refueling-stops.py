aaae9546e6bffa319b12521d9d648327ceafa8c2
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-03 10:30:01 +0545

    initial draft still some test cases failed

--- minimum-number-of-refueling-stops.py
+++ minimum-number-of-refueling-stops.py
@@ -1,1 +1,30 @@
+class Solution(object):
+    def minRefuelStops(self, target, startFuel, stations):
+        """
+        :type target: int
+        :type startFuel: int
+        :type stations: List[List[int]]
+        :rtype: int
+        """
+        if len(stations) == 0:
+            if startFuel < target:
+                return -1
+            else:
+                return 0
+        
+        stops = 0
+        cur = startFuel
+        for dist, fuel in stations:
+            if cur == target:
+                return stops
 
+            if cur < dist:
+                return -1
+            
+            cur += fuel
+            stops += 1
+            
+
+                
+
+        
