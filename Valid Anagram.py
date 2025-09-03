4c84e4f0debdbad34a688503bd4028450ad83786
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-03 11:03:33 +0545

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,1 +1,26 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        present = False
+        j = len(s)
+        k = [i for i in range(j)]
+        if j != len(t):
+            return false
+        for i in range(j):
+            for char in t:
+                if s[i] == char:
+                    if i not in k:
+                        return False
+                    present = True
+                    k.remove(i)
+                    break
+            if not present:
+                return False
+            else:
+                present = False
+        return True
 
