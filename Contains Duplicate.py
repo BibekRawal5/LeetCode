a937428fccec94fd95e64411dd91dcfaf0d94e41
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-05 20:51:31 +0545

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,1 +1,10 @@
 
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        present = {}
+        for n in nums:
+            if present.get(n, 0) == 1:
+                return True
+            present[n] = 1
+        
+        return False
