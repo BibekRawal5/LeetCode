7abbedc7b3e5db1605bf79717b91a42b6cd7dc8c
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-05 20:57:21 +0545

    Update Valid Anagram.py

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
-            return False
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         counter = {}
-        for char in set(s):
-            counter[char] = s.count(char) 
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+        
         for char in t:
-            if char not in counter or counter[char] == 0:
+            if counter.get(char, 0) == 0:
                 return False
             counter[char] -= 1
-        return True 
+        for k in counter:
+            if counter[k] != 0:
+                return False
+        return True
