e76b93b3df875846d4719daac8e6ddc5ed54d0f6
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-05 21:35:03 +0545

    Update Group Anagrams.py

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -27,3 +27,11 @@ class Solution:
             answer.append(cur)
         return answer
                              
+#best solution
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
