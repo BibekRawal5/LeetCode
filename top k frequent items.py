3e0c5970c6c4c7c7f5e8e7d5c8c25c8cd1290a02
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-11 20:27:07 +0545

    Update top k frequent items.py

--- top k frequent items.py
+++ top k frequent items.py
@@ -1,1 +1,18 @@
+#MY
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = defaultdict(int)
+        for n in nums:
+            counter[n] += 1
+        values = sorted(counter.values())
+        n = 0
+        answer = []
+        while n < k:
+            cur_val = values[-1-n]
+            for key,v in counter.items():
+                if v == cur_val:
+                    answer.append(key)
+                    n += 1
+        return answer
+
 
