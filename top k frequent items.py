4c302616e9bd05409513c5623acbc7ab7fb8cec0
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-09-11 20:28:10 +0545

    Update top k frequent items.py

--- top k frequent items.py
+++ top k frequent items.py
@@ -1,6 +1,7 @@
-#MY
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #MY
         counter = defaultdict(int)
         for n in nums:
             counter[n] += 1
@@ -15,4 +16,18 @@ class Solution:
                     n += 1
         return answer
 
+        #bucket sort best answer
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
